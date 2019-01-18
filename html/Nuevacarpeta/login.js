65641f8647b2eeb226dd40f8cccb095a9ba6c15c
Your Name <you@example.com>
2019-01-18 10:01:16 -0300

    edicion de usuario

---
+++ html/Nuevacarpeta/login.js
@@ -0,0 +1,33 @@
+var app = angular.module('login',[]); //estandar para comenzar angular
+
+app.controller('ctrlLogin', function ($scope) { //con esto creamos un controlador
+
+        $scope.mostrarLogin = true;
+        $scope.mostrarError = false;
+        $scope.mensaje = "";
+    
+    
+    
+        $scope.hola = function() {
+    
+    
+    
+            if( $scope.usuario == "" &&  $scope.contrasena== "" ){
+                    $scope.mensaje = "No puede dejar campos vacios";
+                    return ;
+    
+            }    
+            if( ($scope.usuario == "admin" &&  $scope.contrasena=="123456") || $scope.usuario == "nalore1" && $scope.contrasena=="123456") {
+                    $scope.mostrarError = false;
+                    $scope.mostrarLogin = false;  
+                     
+            }else{
+                    $scope.mostrarError = true;
+                    $scope.mostrarLogin = true;
+                    $scope.mensaje = "Usuario y/o Clave incorrectos";
+                }
+    
+        };
+    
+    });
+
